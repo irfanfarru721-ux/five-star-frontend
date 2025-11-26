8fefa222b3d25b07111c8bae8c4ddb0a1d541bd6
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-26 23:58:20 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://multi-vendor-app-ey66.onrender.com/api",
+  baseURL: "https://super-backend-bzin.onrender.com/api",
 });
 
 // Attach Token Automatically
